5a5a940eb06b0beaefce0ccda3021a3125541fc8
Taylor Varoglu <tava8398@colorado.edu>
2021-05-25 19:37:03 -0600

    Fix formatting in lib/score.rb

--- lib/guess.rb
+++ lib/guess.rb
@@ -17,7 +17,7 @@ class Guess
 
   def evaluate_user_input(combo_to_guess, current_guess)
     if current_guess.downcase == 'q' || current_guess.downcase == 'quit'
-    	abort "Game exiting... \n Goodbye!"
+      abort "Game exiting... \n Goodbye!"
     elsif current_guess.downcase == 'c' || current_guess.downcase == 'cheat'
       return combo_to_guess
     elsif current_guess.length < combo_to_guess.length
@@ -25,7 +25,7 @@ class Guess
     elsif current_guess.length > combo_to_guess.length
       return 'Guess is too long. Try again!'
     else
-    	return self.evaluate_user_guess(combo_to_guess, current_guess)
+      return self.evaluate_user_guess(combo_to_guess, current_guess)
     end
   end
 
